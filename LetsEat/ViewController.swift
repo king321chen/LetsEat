d425cd477edb4690c5e94db47d2346400c76779f
King Chen <48711761+king321chen@users.noreply.github.com>
2022-03-18 18:44:47 +0800

    Initial Commit

---
+++ LetsEat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsEat
+//
+//  Created by cobi321 on 2022/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
